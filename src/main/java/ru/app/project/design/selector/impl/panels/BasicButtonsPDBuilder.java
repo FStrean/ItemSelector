a408e9b37ac502049f539095f114b404f7625c90
Daniel <fustone.steamer@mail.ru>
2024-01-11 16:02:37 +0300

    design(Selector): added label design

--- src/main/java/ru/app/project/design/selector/impl/panels/BasicButtonsPDBuilder.java
+++ src/main/java/ru/app/project/design/selector/impl/panels/BasicButtonsPDBuilder.java
@@ -1,6 +1,7 @@
 package ru.app.project.design.selector.impl.panels;
 
 import ru.app.project.components.JImage;
+import ru.app.project.components.RelativeLayout;
 import ru.app.project.design.selector.interf.panels.ButtonsPDBuilder;
 
 import javax.imageio.ImageIO;
@@ -13,15 +14,36 @@ import java.io.IOException;
 
 public class BasicButtonsPDBuilder implements ButtonsPDBuilder {
     private final JPanel panel;
+    private final JPanel buttonsPanel;
 
     public BasicButtonsPDBuilder(JPanel panel) {
         this.panel = panel;
-        this.panel.setLayout(new GridLayout(0,2));
+        this.panel.setLayout(new GridBagLayout());
+        buttonsPanel = new JPanel();
+        RelativeLayout layout = new RelativeLayout(RelativeLayout.X_AXIS, 20);
+        layout.setFill(true);
+        buttonsPanel.setLayout(layout);
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.weightx = 1;
+        constraints.weighty = 0.8;
+        panel.add(buttonsPanel, constraints);
     }
 
     @Override
     public JLabel buildDescription() {
         JLabel label = new JLabel();
+        panel.add(label);
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = 1;
+        constraints.weighty = 0.2;
+        panel.add(label, constraints);
         return label;
     }
 
@@ -44,7 +66,7 @@ public class BasicButtonsPDBuilder implements ButtonsPDBuilder {
         jPanel.setBorder(new EmptyBorder(15, 40, 15, 40));
         jPanel.add(button);
         buttonPanel.add(jPanel);
-        panel.add(buttonPanel);
+        buttonsPanel.add(buttonPanel, 50.0f);
         return button;
     }
 }
